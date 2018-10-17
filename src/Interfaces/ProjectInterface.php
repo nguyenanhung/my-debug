d37b0771f0c7cdfdabfdb9fbbfae5859d5fab04c
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-10-17 12:41:46 +0700

    Fix File.php

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -19,7 +19,7 @@ namespace nguyenanhung\MyDebug\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION = '0.1.2.6';
+    const VERSION = '0.1.2.7';
 
     /**
      * Hàm lấy thông tin phiên bản Packages
