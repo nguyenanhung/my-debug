4dc0e73b669fe76f2ff7765b99425a9556dd62cc
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-11-24 18:53:31 +0700

    Update package

--- src/Interfaces/ProjectInterface.php
+++ src/Interfaces/ProjectInterface.php
@@ -19,7 +19,7 @@ namespace nguyenanhung\MyDebug\Interfaces;
  */
 interface ProjectInterface
 {
-    const VERSION = '0.1.2.8';
+    const VERSION = '0.1.2.9';
 
     /**
      * Hàm lấy thông tin phiên bản Packages
