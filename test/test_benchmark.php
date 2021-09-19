9eb93fa34d0f1f9ef0bb9c7b79cefd791b421929
nguyenanhung <info@nguyenanhung.com>
2021-09-19 22:25:50 +0700

    update test benchmark

--- test/test_benchmark.php
+++ test/test_benchmark.php
@@ -15,9 +15,8 @@ use nguyenanhung\MyDebug\Benchmark;
 $benchmark = new Benchmark();
 /***************************** SIMPLE BENCHMARKING BY CI *****************************/
 $benchmark->mark('code_start');
-$mathFunctions = ["abs", "acos", "asin", "atan", "bindec", "floor", "exp", "sin", "tan", "pi", "is_finite", "is_nan",
-                  "sqrt"];
-$count         = 9999;
+$mathFunctions = ["abs", "acos", "asin", "atan", "floor", "exp", "sin", "tan", "pi", "is_finite", "is_nan","sqrt"];
+$count         = 10;
 for ($i = 0; $i < $count; $i++) {
     foreach ($mathFunctions as $key => $function) {
         $function($i);
