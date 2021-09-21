18f325b8ac0cf1583c80bd90763706501ae360f5
nguyenanhung <info@nguyenanhung.com>
2021-09-22 03:57:18 +0700

    Update benchmark

--- src/Benchmark.php
+++ src/Benchmark.php
@@ -9,6 +9,8 @@
 
 namespace nguyenanhung\MyDebug;
 
+use nguyenanhung\Bear\Benchmark\Benchmark as BearBenchmark;
+
 /**
  * Class Benchmark
  *
@@ -21,87 +23,6 @@ namespace nguyenanhung\MyDebug;
  * @since      2018-10-17
  * @version    0.1.2.5
  */
-class Benchmark implements Project
+class Benchmark extends BearBenchmark
 {
-    use Version;
-
-    /**
-     * Benchmark constructor.
-     *
-     * @author   : 713uk13m <dev@nguyenanhung.com>
-     * @copyright: 713uk13m <dev@nguyenanhung.com>
-     */
-    public function __construct()
-    {
-    }
-
-    /**
-     * List of all benchmark markers
-     *
-     * @var    array
-     */
-    public $marker = array();
-
-    /**
-     * Set a benchmark marker
-     *
-     * Multiple calls to this function can be made so that several
-     * execution points can be timed.
-     *
-     * @param string $name Marker name
-     *
-     * @return    void
-     */
-    public function mark(string $name)
-    {
-        $this->marker[$name] = microtime(TRUE);
-    }
-
-    /**
-     * Elapsed time
-     *
-     * Calculates the time difference between two marked points.
-     *
-     * If the first parameter is empty this function instead returns the
-     * {elapsed_time} pseudo-variable. This permits the full system
-     * execution time to be shown in a template. The output class will
-     * swap the real value for this variable.
-     *
-     * @param string $point1   A particular marked point
-     * @param string $point2   A particular marked point
-     * @param int    $decimals Number of decimal places
-     *
-     * @return    string    Calculated elapsed time on success,
-     *            an '{elapsed_string}' if $point1 is empty
-     *            or an empty string if $point1 is not found.
-     */
-    public function elapsed_time(string $point1 = '', string $point2 = '', int $decimals = 4): string
-    {
-        if ($point1 === '') {
-            return '{elapsed_time}';
-        }
-
-        if (!isset($this->marker[$point1])) {
-            return '';
-        }
-
-        if (!isset($this->marker[$point2])) {
-            $this->marker[$point2] = microtime(TRUE);
-        }
-
-        return number_format($this->marker[$point2] - $this->marker[$point1], $decimals);
-    }
-
-    /**
-     * Function memory_usage
-     *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 10/13/18 10:52
-     *
-     * @return string
-     */
-    public function memory_usage(): string
-    {
-        return round(memory_get_usage() / 1024 / 1024, 2) . 'MB';
-    }
 }
