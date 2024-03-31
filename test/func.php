68465523e6ce173f413fba0fdb8d598946eaccd5
nguyenanhung <info@nguyenanhung.com>
2024-03-31 22:12:36 +0700

    Release version 4.0.3

--- test/func.php
+++ test/func.php
@@ -8,10 +8,10 @@
  * Time: 01:29
  */
 if (!function_exists('__show__')) {
-    function __show__($s)
-    {
-        echo "<pre>";
-        print_r($s);
-        echo "</pre>";
-    }
+	function __show__($s): void
+	{
+		echo "<pre>";
+		print_r($s);
+		echo "</pre>";
+	}
 }
